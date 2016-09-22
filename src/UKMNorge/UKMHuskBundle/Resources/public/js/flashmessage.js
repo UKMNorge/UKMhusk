91798b8f02c2d1f244b467700a9000cb373c52ae
Marius Mandal <mariusmandal@gmail.com>
2016-09-22 19:42:14 +0200

    kjappere flash

--- src/UKMNorge/UKMHuskBundle/Resources/public/js/flashmessage.js
+++ src/UKMNorge/UKMHuskBundle/Resources/public/js/flashmessage.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$('.flashbagMessage').each( function() {
-		setTimeout(function(id){ $('#'+id).slideUp(); }, 4500, $(this).attr('id')); 
+		setTimeout(function(id){ $('#'+id).slideUp(); }, 3500, $(this).attr('id')); 
 	});
 });
